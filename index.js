134d69d1e26ee536825593595ac3fdacc2854e0f
copsmen809 <copsmen809@gmail.com>
2026-07-31 02:53:28 +0700

    Update target URL in fetch function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-    const targetUrl = `https://cdn2.aceimg.com${url.pathname}${url.search}`;
+    const targetUrl = `https://aceimg.com${url.pathname}${url.search}`;
     
     return Response.redirect(targetUrl, 302);
   }
