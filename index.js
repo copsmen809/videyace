40e01c607f97233e2fef5c8c685de03ef201dd04
copsmen809 <copsmen809@gmail.com>
2026-07-30 10:06:35 +0700

    Update target URL format for aceimg redirection

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ export default {
 
     // Gabungkan ke format default aceimg.com dengan parameter ?f=
     // Hasil: https://aceimg.com/upload/?f=16fca76a2.mp4
-    const targetUrl = `cdn2.aceimg.com/${fileName}`;
+    const targetUrl = `cdn2.aceimg.com/upload/?f=${fileName}`;
 
     // Lakukan redirect 302 ke halaman default aceimg
     return Response.redirect(targetUrl, 302);
